8e48b5c0d8c7ffb56d44963898e0adf78e7fa93a
Evgenii Kichakov <kichakov.ea@gmail.com>
2024-04-08 22:26:33 +0300

    layout header

--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
-let a = 15;
 
-let form = document.forms.my;
-let userName = form.elements.name;
-let userAge = form.elements.age;
-console.log(userName, userAge);
-document.write(`<p>${userName.value}</p>`);
 
